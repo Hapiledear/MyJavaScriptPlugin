76cbee231c1bd13d8fc9d94eeb4edcc12b8535d2
yang_huang <yang_huang>
2016-11-25 14:41:25 +0800

    表单修改检测
    0.1版本
    一个在更新表单时可以检测哪些被修改了的小插件

---
+++ changeCheck.js
@@ -0,0 +1,68 @@
+
+	var settings = {
+			objStr :"",
+			hitColor : "#FFC408",
+			primaryColor : "#D5D5D5"
+	};
+	var propArray = [];
+	var valueArray = [];
+	
+	function init(obj){
+		parse(obj);
+		saveOldData(objStr);
+		bindKeyUpEvent();
+		//缺省参数
+	}
+	
+	function parse(obj){
+		if("objStr"	in obj) settings.objStr = obj.objStr;
+		if("hitColor" in obj) settings.hitColor = obj.hitColor;
+		if("primaryColor" in obj) settings.primaryColor = obj.primaryColor;
+	}
+	function saveOldData(dataObj){
+		var regProp = /\w+(?==)/g;
+		var regVal = /[\w\s:\u4e00-\u9fa5]+(?=,|})/g;
+		
+		 propArray = dataObj.match(regProp);
+		 valueArray = dataObj.match(regVal);
+		 
+		for(var i in valueArray){
+			valueArray[i] = formatDate(valueArray[i]);
+		}
+ 		console.log(propArray.toString());
+		console.log(valueArray.toString()); 
+		
+	}
+
+	function formatDate(value){
+		var reg = /\sCST\s/g;
+		if(typeof(value) == "string" && reg.test(value)){
+			return CSTtoUTCe8(value);
+		}
+		return value;
+	}
+	
+	function CSTtoUTCe8(cstStr){
+		var date = cstStr.replace(/\s/g,":").split(":");
+		var utcDate = date[0]+" "+date[1]+" "+date[2]+" "+date[7]+" "+date[3]+":"+date[4]+":"+date[5]+" GMT+0800 (中国标准时间)";
+		return utcDate;
+	}
+	
+	function bindKeyUpEvent(){
+		for(var i in propArray){
+			var $obj = $("[name='"+propArray[i]+"']");
+			$obj.attr("_index",i);
+			
+			$obj.keyup(function(){
+				var newValue = $obj.val();
+				var index = $obj.attr("_index");
+				if(newValue == valueArray[index]){
+					$obj.css({"border-color":settings.primaryColor});
+					$obj.attr("newValue",false);
+				}else{
+					$obj.css({"border-color":settings.hitColor});
+					$obj.attr("newValue",true);
+				}
+			});
+		}
+	}
